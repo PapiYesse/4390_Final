b9a5d2a388354fa3d8180584eada1ef64ae81335
Dawn Scott <89325001+dmscott2@users.noreply.github.com>
2022-05-01 15:23:00 -0500

    Dawn_CreateFormUpdates

--- webapp/Pages/Assessments/Create.cshtml.cs
+++ webapp/Pages/Assessments/Create.cshtml.cs
@@ -12,6 +12,7 @@ namespace webapp.Pages.Assessments
     public class CreateModel : PageModel
     {
         private readonly PARDbContext _context;
+        private readonly ILogger<CreateModel> _logger;
 
         public CreateModel(PARDbContext context)
         {
@@ -21,10 +22,19 @@ namespace webapp.Pages.Assessments
         public IActionResult OnGet()
         {
             return Page();
+            
         }
 
         [BindProperty]
         public Assessment Assessment { get; set; }
+        public Course Course {get; set; }
+        public AcademicYearPeriod AcademicYearPeriod {get; set; }
+        public SelectList CoursesDropDown {get; set;} 
+        public SelectList CoordinatorDropDown {get; set;} 
+        public SelectList CatalogDescriptionDropDown {get; set;}
+        public SelectList YearDropDown {get; set; }
+        public SelectList SemesterDropDown {get; set; }
+        public SelectList InstructorDropDown {get; set; }
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
